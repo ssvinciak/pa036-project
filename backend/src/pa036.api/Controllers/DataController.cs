152f8b150370fff40a76fe82b6a6f03f6637a148
Stefan Svinciak <ssvinciak@acmark.cz>
2019-04-20 10:04:24 +0200

    added solution for backend

---
+++ backend/src/pa036.api/Controllers/DataController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace pa036.api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DataController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok("test");
+        }
+    }
+}
