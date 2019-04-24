ac993e6d9e44aedb5d58dcd6bc6f5ddb32ecc8ff
Stefan Svinciak <ssvinciak@acmark.cz>
2019-04-24 22:32:02 +0200

    added database project

---
+++ backend/pa036.db/DataDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using pa036.db.Entities;
+
+namespace pa036.db
+{
+    public class DataDbContext : DbContext
+    {
+        public DataDbContext()
+        {
+            // TODO: 
+            // 2 options for default data
+            // 1. here add code for parsing data from csv and store to db
+            // 2. add migration with data
+        }
+
+        public DbSet<Measurement> Measurements { get; set; }
+    }
+}
---
+++ backend/pa036.db/Entities/Measurement.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace pa036.db.Entities
+{
+    public class Measurement
+    {
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        [Key]
+        public int Key { get; set; }
+
+        // TODO: add other properties for
+    }
+}
