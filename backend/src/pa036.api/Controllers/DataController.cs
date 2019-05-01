7224f29051f682014151580b591d5efccf495653
Miriama Sasaráková <424615@mail.muni.cz>
2019-05-01 18:57:17 +0200

    api params

--- backend/src/pa036.api/Controllers/DataController.cs
+++ backend/src/pa036.api/Controllers/DataController.cs
@@ -15,14 +15,16 @@ namespace pa036.api.Controllers
     public class DataController : ControllerBase
     {
         [HttpGet]
-        public JsonResult Get()
+        public JsonResult Get(string from, string to)
         {
+            DateTime.TryParse(from, out var fromDate);
+            DateTime.TryParse(to, out var toDate);
             using (var context = new DataDbContext())
             {
                 var watch = System.Diagnostics.Stopwatch.StartNew();
                 var data = context
                     .Measurements
-                    .Where(s => s.MeasurementDate.Year == 1985)
+                    .Where(s => s.MeasurementDate >= fromDate && s.MeasurementDate <= toDate)
                     .AsNoTracking()
                     .ToList();
                 watch.Stop();
