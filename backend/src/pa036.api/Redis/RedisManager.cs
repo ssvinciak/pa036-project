f9d1ebdaf90d0d090338da128c5001aab983d27b
Stefan Svinciak <ssvinciak@acmark.cz>
2019-05-01 12:39:43 +0200

    added redis cache to project

---
+++ backend/src/pa036.api/Redis/RedisManager.cs
@@ -0,0 +1,34 @@
+﻿using StackExchange.Redis;
+
+namespace pa036.api.Redis
+{
+    public class RedisManager
+    {
+        private static ConnectionMultiplexer _instance;
+        private static readonly object _lockObject = new object();
+
+        public static ConnectionMultiplexer Instance
+        {
+            get
+            {
+                if (_instance == null)
+                {
+                    lock (_lockObject)
+                    {
+                        if (_instance == null || !_instance.IsConnected)
+                        {
+                            _instance = ConnectionMultiplexer.Connect("localhost");
+                        }
+                    }
+                }
+
+                return _instance;
+            }
+        }
+
+        public static IDatabase GetDatabase()
+        {
+            return _instance?.GetDatabase();
+        }
+    }
+}
