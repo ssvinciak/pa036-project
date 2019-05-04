4aef2665ae3f755d7cc311a9aeea6cc7304fb706
Stefan Svinciak <ssvinciak@acmark.cz>
2019-05-04 13:44:36 +0200

    modified default time settings

--- frontend/src/constants/defaultTimeSettings.ts
+++ frontend/src/constants/defaultTimeSettings.ts
@@ -1,9 +1,9 @@
-export const defaultFromDate = new Date(2000, 1 , 1);
+export const defaultFromDate = new Date(2000, 1, 1);
 
 export const defaultToDate = new Date(2000, 1, 2);
 
-export const defaultReloadTime = 10;
+export const defaultReloadTime = 5;
 
 export const defaultEFCache = true;
 
-export const defaultRedisCache = true;
+export const defaultRedisCache = false;
