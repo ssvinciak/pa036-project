ee504eaaca2e2f32ce59ccfd15dd5d7c4e48a05c
Stefan Svinciak <stefan.svinciak@gmail.com>
2019-05-12 11:03:17 +0200

    fixed cached versions

--- frontend/src/constants/cacheVersions.ts
+++ frontend/src/constants/cacheVersions.ts
@@ -1,28 +1,22 @@
-export const specialOne = 5;
-
-export const redisOn_efOn = 4;
-
-export const redisOn_efOff = 3;
-
-export const redisOff_efOn = 2;
-
-export const redisOff_efOff = 1;
-
 export const cacheOptions = [
     {
-        value: '4',
-        label: 'Redis on, EF on',
+        value: 1,
+        label: 'Redis off, EF off',
     },
     {
-        value: '3',
-        label: 'Redis on, EF off',
-    },
-    {
-        value: '2',
+        value: 2,
         label: 'Redis off, EF on',
     },
     {
-        value: '1',
-        label: 'Redis off, EF off',
+        value: 3,
+        label: 'Redis on, EF off',
+    },
+    {
+        value: 4,
+        label: 'Redis on, EF on',
+    },
+    {
+        value: 5,
+        label: 'Sorted set range',
     },
 ];
