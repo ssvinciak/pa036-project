9362160518b39e7137e16a603d5131e8205322b2
Stefan Svinciak <stefan.svinciak@gmail.com>
2019-05-12 11:03:06 +0200

    removed unused packages and jest

--- frontend/jest.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  transform: {
-    "^.+\\.jsx?$": "babel-jest",
-    "^.+\\.tsx?$": "ts-jest",
-  },
-  testRegex: "(^..+\\.tests?\\.(j|t)sx?$)|(^.+/__(tests?|specs?)__/.+\\.(j|t)sx?$)",
-  moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"]
-};
